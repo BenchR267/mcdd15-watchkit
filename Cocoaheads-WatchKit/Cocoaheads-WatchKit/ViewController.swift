da28f8a9dca93dae22e7af591ad2aaec8b7887bb
Benjamin Herzog <hallo@benchr.de>
2015-04-06 10:59:24 +0200

    Initial Xcode commit

---
+++ Cocoaheads-WatchKit/Cocoaheads-WatchKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cocoaheads-WatchKit
+//
+//  Created by Benjamin Herzog on 06.04.15.
+//  Copyright (c) 2015 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
